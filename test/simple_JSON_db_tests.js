55ea12bc9f3893993345e7a7e2a264014cd4eabc
kgentner <gentnerkarl@gmail.com>
2014-11-08 22:31:59 -0800

    mkdir to before script - brought back write and read tests

--- test/simple_JSON_db_tests.js
+++ test/simple_JSON_db_tests.js
@@ -9,25 +9,24 @@ require('../server');
 var expect = chai.expect;
 
 describe('simple JSON Database', function() {
-  it('should be able to write json', function(done) {
+  it('should be able to write a single user', function(done) {
     chai.request('http://localhost:8080')
-    .post('/test')
+    .post('/testname')
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res).to.be.json;
-      //expect(res.body.msg).to.eql('New User Saved!');
+      expect(res.body.msg).to.eql('New User Saved!');
       done();
     });
   });
 
-  // it('should be able to get a single user', function(done) {
-  //   chai.request('http://localhost:8080')
-  //   .get('/karl')
-  //   .end(function(err, res) {
-  //     expect(err).to.eql(null);
-  //     expect(res.body.name).to.eql('karl');
-  //     done();
-  //   });
-  // });
+  it('should be able to get a single user', function(done) {
+    chai.request('http://localhost:8080')
+    .get('/testname')
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.name).to.eql('testname');
+      done();
+    });
+  });
 
 });
