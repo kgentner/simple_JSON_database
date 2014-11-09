aa7b0a7bf4c1b0abbda91cb771920ab6507a2dcd
kgentner <gentnerkarl@gmail.com>
2014-11-08 19:51:07 -0800

    updated server and package.json

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/:username', function(req, res) {
+  var user = req.params.username;
+  var filename = user.concat('.json');
+  var filepath = __dirname + '/data/' + filename;
+
+  res.sendFile(filepath);
+});
+
+app.post('/:username', function(req, res) {
+  var user = req.params.username;
+  var data = ('{\r\n  "name": ' + '"' + user + '"\r\n}');
+  var filename = user.concat('.json');
+  var filepath = __dirname + '/data/' + filename;
+
+  fs.writeFile(filepath, data, function(err) {
+    if (err) { throw err; }
+    console.log('A new user has been saved!');
+  });
+  res.send('It\'s saved!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('server running on port: %d', app.get('port'));
+});
